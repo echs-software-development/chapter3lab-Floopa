1ff3642eea857b7cfef2e99cf696836bc9ea05f1
Aleksandra <ab0200@students.necsd.net>
2020-12-12 01:01:40 +0000

    prob3

--- Prob1.java
+++ Prob1.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 
 public class Prob1 {
- public static void main(String[] args)
-{
-Scanner scan = new Scanner(System.in);
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
 
-System.out.println("Enter a number:");
+    System.out.println("Enter a number:");
 
-int num = scan.nextInt();
+    int num = scan.nextInt();
 
-if ( !( num >= 5 && num <= 76))
-
-System.out.println("True");
-
-else
-System.out.println("False");
+    if ( !( num >= 5 && num <= 76))
+      System.out.println("True");
+    else
+      System.out.println("False");
 
   }
-  }
+}
--- Prob3.java
+++ Prob3.java
@@ -2,25 +2,23 @@ import java.util.Scanner;
 
 public class Prob3 {
   public static void main(String[] args) {
-    
-    
-    // Prompt the user to enter weight in pounds
-    
-    
-    // Prompt the user to enter height in inches
+		Scanner input = new Scanner(System.in);
 
+		final double KILOGRAMS_PER_POUND = 0.45359237;
+		final double METERS_PER_INCHES = 0.0254;
 
-    
-    //Constant
-    final double KILOGRAMS_PER_POUND = 0.45359237; 
-    final double METERS_PER_INCH = 0.0254; 
-    
-    // Compute BMI
-    double weightInKilograms = weight * KILOGRAMS_PER_POUND;
-    double heightInMeters = height * METERS_PER_INCH;
-    double BMI = weightInKilograms/(heightInMeters * heightInMeters);
+		System.out.println("Enter weight in pounds:");
+		double weight = input.nextDouble();
 
-    // Display result using if, else if and else
-   
-  }
+		System.out.println("Enter height in inches:");
+		double height = input.nextDouble();
+
+		weight = weight * KILOGRAMS_PER_POUND;
+
+		height = height * Meters_PER_INCHES;
+    //i was having trouble with the statment below, I eventually asked my dad and he reccomended using Math.pow even though i seem to remeber us doing it differently in class
+		double bodyMassIndex = weight / Math.pow(height, 2);
+
+		System.out.println("BMI is " + bodyMassIndex);
+	}
 }
